3382540cc1d274fbe0c519a0dffece63a238205a
Jessica-Gualtieri <jessicagualtieri@hotmail.fr>
2024-10-03 18:18:25 +0200

    ✨ New: created my first component in Angular : a game card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GameCardComponent } from './components/game-card/game-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [GameCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'games-collection';
+  
 }
